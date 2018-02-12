a32c15d3205c6fd20af74970808f40697b093154
Confringe <35739529+Confringe@users.noreply.github.com>
2018-02-12 19:45:44 +0100

    Work in Progress

--- keyboards/lets_split/keymaps/confringe/keymap.c
+++ keyboards/lets_split/keymaps/confringe/keymap.c
@@ -12,16 +12,32 @@ extern keymap_config_t keymap_config;
 #define _FN1 1
 #define _FN2 2
 #define _FN3 3
+#define _numpad 16
 
 enum custom_keycodes {
   DVORAK = SAFE_RANGE,
   FN1,
   FN2,
   FN3,
+  numpad,
 };
 
-enum {
+enum {    //macro
   okwow=SAFE_RANGE,
+};
+
+enum {    //tapdance
+  commadot=0,
+  curlybrace,
+  paren,
+  brace
+};
+
+qk_tap_dance_action_t tap_dance_actions[]={
+  [commadot]=ACTION_TAP_DANCE_DOUBLE(KC_PCMM, KC_PDOT),
+  [curlybrace]=ACTION_TAP_DANCE_DOUBLE(KC_LCBR, KC_RCBR),
+  [paren]=ACTION_TAP_DANCE_DOUBLE(KC_LPRN, KC_RPRN),
+  [brace]=ACTION_TAP_DANCE_DOUBLE(KC_LBRC, KC_RBRC)
 };
 
 // Fillers to make layering more clear
@@ -44,11 +60,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_MINS, \
   KC_TAB,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_ENT, \
   KC_LALT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_SLSH, \
-  KC_LCTL,  KC_LGUI, DF(_FN3), DF(_FN2), DF(_FN1),   SFT_T(KC_SPC),  KC_BSPC,  KC_DELETE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+  KC_LCTL,  KC_LGUI, _FN3, _FN2, _FN1, SFT_T(KC_SPC), KC_BSPC, KC_DELETE, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT \
 ),
 
 /* FN1
- * ,-----------------------------------------------------------------------------------.5
+ * ,-----------------------------------------------------------------------------------.
  * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Del  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |      |   \  |  |   |
@@ -59,10 +75,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_FN1] = KEYMAP( \
-  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL, \
-  KC_DEL,  KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
+  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, TD(paren), TD(curlybrace), TD(brace), KC_DEL, \
+  KC_TAB,  KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7, KC_8, KC_9, KC_0, KC_PIPE, \
   okwow,   KC_F23,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS),S(KC_NUBS),_______, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
+  _______, _______, _______, _______, DVORAK, SFT_T(KC_SPC), KC_BSPC, KC_DELETE, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT \
 ),
 
 /* FN2
@@ -88,6 +104,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_A, KC_B, KC_C, KC_D, KC_E, KC_F, KC_G, KC_H, KC_I, KC_J, KC_K, KC_L,
   KC_A, KC_B, KC_C, KC_D, KC_E, KC_F, KC_G, KC_H, KC_I, KC_J, KC_K, KC_L,
   KC_A, KC_B, KC_C, KC_D, KC_E, KC_F, KC_G, KC_H, KC_I, KC_J, KC_K, KC_L
+),
+
+[_numpad] = KEYMAP(\
+  KC_NLCK, _______, KC_PMNS, KC_P7, KC_P8, KC_P9,        KC_P7, KC_P8, KC_P9, KC_PMNS, _______, KC_NLCK,
+  _______, KC_BSPC, KC_PPLS, KC_P4, KC_P5, KC_P6,        KC_P4, KC_P5, KC_P6, KC_PPLS, KC_BSPC, _______,
+  _______, _______, KC_PAST, KC_P3, KC_P2, KC_P1,        KC_P3, KC_P2, KC_P1, KC_PAST, _______, _______,
+  DVORAK, _______, KC_PSLS, KC_P0, TD(commadot), KC_PENT,  KC_P0, TD(commadot), KC_PENT, KC_PSLS, _______, DVORAK
 )
 
 };
@@ -122,10 +145,18 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       return false;
       break;
     case FN3:
-      if (record->eveent.pressed) {
+      if (record->event.pressed) {
         layer_on(_FN3);
       } else {
         layer_off(_FN3);
+      }
+      return false;
+      break;
+    case numpad:
+      if (record->event.pressed) {
+        layer_on(_numpad);
+      } else {
+        layer_off(_numpad);
       }
       return false;
       break;
